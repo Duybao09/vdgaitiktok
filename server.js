073f54f7ba4a665353c322a0df37be2ff662191c
Duybao09 <buiduybao2024@gmail.com>
2026-02-25 18:55:11 +0700

    server.js

--- server.js
+++ server.js
@@ -38,7 +38,21 @@ const videoGai = [
     "https://image2url.com/r2/default/videos/1772015581843-0246e54f-9d4f-482e-b418-f36b9f1df567.mp4",
     "https://image2url.com/r2/default/videos/1772015622280-9c6f2166-190f-4fe1-a440-8aed33628140.mp4",
     "https://image2url.com/r2/default/videos/1772015644220-de4bc53c-bf1d-4bac-890b-4c50096f672a.mp4",
-    "https://image2url.com/r2/default/videos/1772015664504-6d84c350-2f43-4a09-97c3-0d5b126cd49f.mp4"
+    "https://image2url.com/r2/default/videos/1772015664504-6d84c350-2f43-4a09-97c3-0d5b126cd49f.mp4",
+    "https://image2url.com/r2/default/videos/1772019286892-41d8ff18-ca49-47bc-be8c-1b3eba4c8b05.mp4",
+    "https://image2url.com/r2/default/videos/1772019391526-5930e0c5-c054-4dc4-b6ad-b91b160cd1be.mp4",
+    "https://image2url.com/r2/default/videos/1772019410867-e782ae3a-6312-4a8a-a614-44c21d5d516e.mp4",
+    "https://image2url.com/r2/default/videos/1772019447386-fa545b79-399e-491f-a8e4-457454d81b7e.mp4",
+    "https://image2url.com/r2/default/videos/1772019469086-e67ee9a9-5d08-457b-8063-adceeac3f0ab.mp4",
+    "https://image2url.com/r2/default/videos/1772019491104-514630e0-fd23-4983-9574-d1a600173998.mp4",
+    "https://image2url.com/r2/default/videos/1772019537085-132db6b4-21c6-4016-9d61-3e78a524a573.mp4",
+    "https://image2url.com/r2/default/videos/1772019553530-95acd388-1d06-4f6a-b08e-d59765bee3f1.mp4",
+    "https://image2url.com/r2/default/videos/1772019570784-5ad9e547-cdca-448f-bca0-ae79451a2029.mp4",
+    "https://image2url.com/r2/default/videos/1772019588853-c3c33b79-4f6e-40ee-a6f0-42592fef3b28.mp4",
+    "https://image2url.com/r2/default/videos/1772019605533-f85120f5-7cf0-401e-8ce8-caf006e5b93e.mp4",
+    "https://image2url.com/r2/default/videos/1772019621150-65d7bc96-df92-41d9-b320-f4c039b6ab59.mp4",
+    "https://image2url.com/r2/default/videos/1772020463287-0ea2b389-cdec-4797-8de4-4f85d42b21f2.mp4",
+    "https://image2url.com/r2/default/videos/1772020494654-75e4cbac-21b8-4d4f-94d8-34f7c6502122.mp4"
 ];
 
 /* =========================
@@ -58,7 +72,7 @@ app.get("/api/gai", (req, res) => {
 
     res.json({
         status: true,
-        author: "Duy Bảo",
+        author: " API BY Duy Bảo",
         total_video: videoGai.length,
         video_url: video
     });
